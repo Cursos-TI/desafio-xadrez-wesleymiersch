5baa1c95d6ab7988ab45a9e06ee806f5d3fcc4d6
wesleymiersch <miersch.wesley@gmail.com>
2026-08-08 20:06:46 +0000

    implementacao de movimentação de peças

--- xadrez.c
+++ xadrez.c
@@ -5,18 +5,27 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+    int torre=1, rainha=1;
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    for (int bispo=1; bispo<=5; bispo++){
+        printf ("Bispo uma casa para cima a direita\n");
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    while( torre <=5){
+        printf (" Torre uma casa para Cima \n");
+        torre++;
+    }
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    do  {
+        printf ("Rainha para a esquerda\n");
+        rainha++;
+    }while (rainha<=8);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
